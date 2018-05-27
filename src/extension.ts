4f042d90905140c0a37b615f0d7e9859390a678d
Per Persson <md2perpe@gmail.com>
2018-05-27 18:18:23 +0200

    Save GitHub token to global settings instead of specific file

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { DepNodeProvider } from './filesList';
-import * as fs from 'fs-extra';
 import {store} from './store';
 import { sendReview } from './commands/sendReview';
 import { showDiff } from './commands/showDiff';
@@ -17,25 +16,12 @@ import { provider } from './providers/commentsReviewer';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-    try {
-        const token = await fs.readFile(`${process.env.HOME}/.githubtoken`);
-        store.githubToken = token.toString();
-    } catch(e){
-        console.error(e);
-    }
-  
+    const token = vscode.workspace.getConfiguration("pullRequester").get("githubToken") as string;
+    store.githubToken = token.toString();
+
     const registration = vscode.workspace.registerTextDocumentContentProvider('css-preview', provider);
     context.subscriptions.push(registration);
     vscode.commands.registerCommand('pullRequester.readComments', readAllFileComments);
-<<<<<<< HEAD
-
-    vscode.commands.registerCommand('pullRequester.sendPullRequestReview', sendReview);
-    vscode.commands.registerCommand('pullRequester.showDiff', showDiff);
-    vscode.commands.registerCommand('pullRequester.viewPull', viewPullInBrowser);
-    vscode.commands.registerCommand('pullRequester.selectPullRequest', async () => {
-       const pullsFiles = await selectPullRequest();
-       vscode.window.registerTreeDataProvider('nodeDependencies', new DepNodeProvider(pullsFiles));
-=======
 
     vscode.commands.registerCommand('pullRequester.sendPullRequestReview', sendReview);
     vscode.commands.registerCommand('pullRequester.showDiff', showDiff);
@@ -47,9 +33,7 @@ export async function activate(context: vscode.ExtensionContext) {
         } catch(e) {
             vscode.window.showErrorMessage(e.message);
         }
-
->>>>>>> yossi/master
-    })
+    });
 
     vscode.commands.registerCommand('pullRequester.addToken', addGithubToken);
     vscode.commands.registerCommand('pullRequester.addComment', addPullRequestComment);
