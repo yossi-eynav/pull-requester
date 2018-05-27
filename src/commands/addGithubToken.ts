c525047238a332d6dffe24fe4b2ccb27295373ca
Per Persson <md2perpe@gmail.com>
2018-05-27 17:02:03 +0200

    Add missing (recommended) semicolon

--- src/commands/addGithubToken.ts
+++ src/commands/addGithubToken.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs-extra';
 
 export async function addGithubToken() {
-    const token = await vscode.window.showInputBox({password: true, prompt: 'Enter a valid github token:'})
+    const token = await vscode.window.showInputBox({password: true, prompt: 'Enter a valid github token:'});
     await fs.outputFile(`${process.env.HOME}/.githubtoken`, token);
 
     vscode.window.showInformationMessage('Github token has been saved.');
