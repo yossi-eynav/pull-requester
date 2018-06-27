ca1d09c413844adee1554d23c91793a08ae47a91
Netanel Ben <netanelben@users.noreply.github.com>
2018-06-28 01:21:30 +0300

    [FEAT] add comments preview page (#8)

--- src/providers/commentsReviewer.ts
+++ src/providers/commentsReviewer.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
-import { store } from '../store';
-import * as escape from 'escape-html';
 import fetch from 'node-fetch';
+import { store } from '../store';
 
 export class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
     private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
@@ -13,8 +12,8 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
         const file = editor.document.uri.path.replace('/tmp/', '');
         const token = store.githubToken;
 
-        const commentsRequests = await fetch(store.currentPullRequest.url + `/comments?access_token=${token}`)
-        const comments = await commentsRequests.json()
+        const commentsRequests = await fetch(store.currentPullRequest.url + `/comments?access_token=${token}`);
+        const comments = await commentsRequests.json();
         
         return this.snippet(comments.filter(c => c.path === file));
     }
@@ -28,55 +27,117 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
     }
 
     private snippet(comments: any): string {
-        
         return `
         <head>
-        
+            <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+            <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+            <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
         </head>
         <style>
-      
-       .body {
-            font-size: 16px;
-            line-height: 16px;
-            padding: 7px;
-            background-color: white;
-            margin-bottom: 10px;
-            color: black;
-       }
+            @import url('https://fonts.googleapis.com/css?family=Sunflower:300');
 
-       body {
-        background-color: white;
-        color: black;
-       }
+            body {
+                font-family: 'Sunflower', sans-serif;
+                color: #000;
+            }
+            
+            .comments {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+                padding: 10px;
+            }
+            
+            .comments article {
+                background: #fff;
+                text-align: left;
+                border: 1px solid #d6d6d6;
+                box-shadow: 3px 3px 0px #bababa;
+                border-radius: 3px;
+                padding: 15px 20px;
+                margin-bottom: 20px;
+            }
+            
+            .comments header,
+            .comments footer,
+            .comments .user {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+            
+            .comments img {
+                width: 50px;
+                height: 50px;
+                border-radius: 50%;
+                margin-right: 10px;
+            }
+            
+            .comments pre {
+                background: #f7f7f7;
+                white-space: pre-wrap;
+                margin: 40px auto;
+                padding: 8px;
+            }
+            
+            .comments span {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        </style>
 
-       .body span {
-            white-space: nowrap;
-       }
+        <body>
+            <div id="root"></div>
 
-       .body strong {
-           display: block;
-       }
+            <script type="text/babel">
+                const commentList = ${JSON.stringify(comments)};
 
-       .item {
-           padding: 5px;
-           border: 1px solid rgba(0,0,0,0.2);
-           margin-bottom: 10px;
-       }
-            </style>
-            <body>
-                ${comments.map(c => {
-                    let body = escape(c.diff_hunk);
+                function CommentCard({ comment }) {
+                    const date = new Date(comment.created_at).toDateString();
+                    const commentText = comment.body.replace(/(@\w*)/g, '<a href="//github.com/$1" target="_blank">$1</a>');
+                    const diffHunk = comment.diff_hunk;
+                
+                    return (
+                        <article id={comment.id}>
+                            <header>
+                                <a href={comment.user.html_url} className="user" target="_blank">
+                                    <img src={comment.user.avatar_url}/>
+                                    <span>{comment.user.login}</span>
+                                </a>
+                                <span>{date}</span>
+                            </header>
+                    
+                            <pre dangerouslySetInnerHTML={{ __html: commentText }}></pre>
 
-                    return `<div class="item">
-                        <pre><code>${body}</code></pre>
-                        <p class="body">
-                            <span>"${escape(c.body)}"</span>
-                            <strong>@${c.user.login}</strong>
-                        </p>
-                    </div>`;
-                })}
-               
-            </body>`;
+                            <pre>
+                                <code dangerouslySetInnerHTML={{ __html: diffHunk }}></code>
+                            </pre>
+                    
+                            <footer>
+                                <span>{comment.path}</span>
+                                <a href={comment.html_url} target="_blank">Comment Link</a>
+                            </footer>
+                        </article>
+                    );
+                }
+                
+                function CommentCardContainer() {
+                    return (
+                        <div className="comments">
+                            <a href={commentList[0].pull_request_url} target="_blank">Pull Request</a>
+                            {commentList.map((comment) => {
+                                return <CommentCard comment={comment} key={comment.id}/>;
+                            })}
+                        </div>
+                    );
+                }
+
+                ReactDOM.render(<CommentCardContainer/>, document.getElementById('root'));
+            </script>
+       
+        </body>
+        `;
     }
 }
 
