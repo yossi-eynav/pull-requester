7aefdf1b73459c6a4da97d7908c3b4e7b827f257
shlomiuziel <shlomi.uziel@gmail.com>
2018-08-19 23:35:57 +0300

    Fixed homedir reference to support windows

--- src/commands/addGithubToken.ts
+++ src/commands/addGithubToken.ts
@@ -6,8 +6,10 @@ export async function addGithubToken() {
     const token = await vscode.window.showInputBox({password: true, prompt: 'Enter a valid github token:'})
     if (!token) { return; }
 
+    const homedir = require('os').homedir();
+
     await vscode.workspace.getConfiguration("pullRequester").update("githubToken", token, vscode.ConfigurationTarget.Global);
-    await fs.outputFile(`${process.env.HOME}/.githubtoken`, token);
+    await fs.outputFile(`${homedir}/.githubtoken`, token);
     store.githubToken = token;
     vscode.window.showInformationMessage('Github token has been saved.');
 }
